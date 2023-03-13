93f8ec7bf682d2701b2f1e8f18810f0a30b1203a
郭乙江 <guoyijiang-xf@bestpay.com.cn>
2023-03-13 11:09:30 +0800

    配置tsconfig

--- src/common/appRouter.ts
+++ src/common/appRouter.ts
@@ -1,8 +1,6 @@
-import {createRouter, createWebHashHistory} from "vue-router";
+import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
 
-
-
-const routers = [
+const routers: RouteRecordRaw[] = [
     {
         path: "/",
         redirect: '/home'
@@ -14,7 +12,7 @@ const routers = [
     },
     {
         path: "/space",
-        component: () =>　import("../components/Space.vue"),
+        component: () =>　import("../components/Home.vue"),
         name: "Club Space",
     },
 ]
